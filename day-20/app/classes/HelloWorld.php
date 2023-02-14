48bd383007038a6d5ab394635648631765d1652b
Mehedi Ridoy <53517557+mhridoy01@users.noreply.github.com>
2023-02-14 16:49:48 +0600

    Add files via upload

---
+++ day-20/app/classes/HelloWorld.php
@@ -0,0 +1,122 @@
+<?php
+
+
+namespace App\classes;
+
+
+class HelloWorld
+{
+//     public function index()
+//     {
+//         echo 'Hello World';
+//     }
+
+        public $firstNumber, $middleNumber, $lastNumber;
+
+        public function index()
+        {
+            /*
+             *           statement
+             *              3 type
+             *              single line statement
+             *              conditional statement
+             *                  if
+             *                  if else
+             *                  if else if
+             *                  switch
+             *
+             *             repeated statement
+             *                  for
+             *                  while
+             *                  do while
+             *                  for eachs
+             * syntax
+             * keyword
+             * value
+             *      fixed value
+             *      variable value
+             * operator
+             * expression
+             *
+             */
+//            $b=20;
+            $this->firstNumber = 10;
+//            $this->firstNumber = $b;
+
+
+
+
+
+//            $this-> firstNumber =30;
+//            $this-> middleNumber=15;
+//            $this->lastNumber = 20;
+//
+//
+//
+//            /*
+//             * logical operator (&&, ||, !)
+//             */
+//
+//            $a = false;
+//            echo !$a;
+
+//            echo '<br/>';
+//            echo($this->firstNumber < $this->lastNumber) && ($this->middleNumber < $this->lastNumber);
+//            echo '<br/>';
+//            echo($this->firstNumber < $this->lastNumber) && ($this->middleNumber > $this->lastNumber);
+//            echo '<br/>';
+//            echo($this->firstNumber > $this->lastNumber) && ($this->middleNumber < $this->lastNumber);
+//            echo '<br/>';
+//            echo($this->firstNumber > $this->lastNumber) && ($this->middleNumber > $this->lastNumber);
+//
+//            echo '<br/>';
+//            echo($this->firstNumber < $this->lastNumber) || ($this->middleNumber < $this->lastNumber);
+//            echo '<br/>';
+//            echo($this->firstNumber < $this->lastNumber) || ($this->middleNumber > $this->lastNumber);
+//            echo '<br/>';
+//            echo($this->firstNumber > $this->lastNumber) || ($this->middleNumber < $this->lastNumber);
+//            echo '<br/>';
+//            echo($this->firstNumber > $this->lastNumber) || ($this->middleNumber > $this->lastNumber);
+
+
+
+            /*
+             *
+             * conditional operator: (>,>=,<,<=,==,===,!=,!==)
+             */
+
+//            echo '<br/>';
+//            echo $this->firstNumber > $this->lastNumber; //true
+//            echo '<br/>';
+//            echo $this->firstNumber >= $this->lastNumber; //false
+//            echo '<br/>';
+//            echo $this->firstNumber < $this->lastNumber;
+//            echo '<br/>';
+//            echo $this->firstNumber <= $this->lastNumber;
+//            echo '<br/>';
+//            echo $this->firstNumber == $this->lastNumber;
+//            echo '<br/>';
+//            echo $this->firstNumber === $this->lastNumber;
+//            echo '<br/>';
+//            echo $this->firstNumber != $this->lastNumber;
+//            echo '<br/>';
+//            echo $this->firstNumber !== $this->lastNumber;
+
+
+
+
+
+//            echo '<br/>';
+//            echo $this->firstNumber += $this->lastNumber; //50
+//            echo '<br/>';
+//            echo $this->firstNumber -= $this->lastNumber; //30
+//            echo '<br/>';
+//            echo $this->firstNumber *= $this->lastNumber;  //600
+//            echo '<br/>';
+//            echo $this->firstNumber /= $this->lastNumber;  //30
+//            echo '<br/>';
+//            echo $this->firstNumber %= $this->lastNumber;  //10
+//            echo '<br/>';
+//            echo $this->firstNumber .= $this->lastNumber;  //1020
+        }
+}
